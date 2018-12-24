299a251591dcec92ca5c6c9395063039996331d5
Ivan Garcia <ivanvlora@gmail.com>
2018-12-24 18:51:23 +0900

    Updates

---
+++ precache-manifest.81f09fd6a9f6bbe12e4f000146b1d628.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "391c3ac23e2a97a6d232",
+    "url": "/youtube-search/static/js/runtime~main.391c3ac2.js"
+  },
+  {
+    "revision": "15ce29ef7111393fc7c7",
+    "url": "/youtube-search/static/js/main.15ce29ef.chunk.js"
+  },
+  {
+    "revision": "a8ca30bfee102f5eda36",
+    "url": "/youtube-search/static/js/1.a8ca30bf.chunk.js"
+  },
+  {
+    "revision": "15ce29ef7111393fc7c7",
+    "url": "/youtube-search/static/css/main.f3400975.chunk.css"
+  },
+  {
+    "revision": "6e059101ad2af176a6cec2fbaf2e1b64",
+    "url": "/youtube-search/index.html"
+  }
+];
